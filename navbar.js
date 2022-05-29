aa3603f235957953ce30b489b9f87425c1ca4136
Gtadia <m235humit9@gmail.com>
2022-05-29 16:39:54 -0400

    Just saving

---
+++ navbar.js
@@ -0,0 +1,5 @@
+window.addEventListener('scroll', function () {
+    let header = document.querySelector('nav');
+    let windowPosition = window.scrollY > 100;
+    header.classList.toggle('scrolling-active', windowPosition);
+})
