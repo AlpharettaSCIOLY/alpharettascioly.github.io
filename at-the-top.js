57ba1f9772d7bc4d91244f30d14fed5b3196045b
Gtadia <m235humit9@gmail.com>
2022-06-03 17:00:51 -0400

    Nearly Finished the events page

--- at-the-top.js
+++
@@ -1,5 +0,0 @@
-window.addEventListener('scroll', function () {
-    let header = document.querySelector('.stuff');
-    let windowPosition = window.scrollY <= 100;
-    header.classList.toggle('scrolling-deactivated', windowPosition);
-})
---
+++ see-more-detail.js
@@ -0,0 +1,25 @@
+var seeMore;
+var article;
+
+var num; 
+
+var buttons = document.getElementsByTagName("button");
+var buttonsCount = buttons.length;
+for (var i = 0; i <= buttonsCount; i += 1) {
+    buttons[i].onclick = function(e) {
+        num = 1 * (this.id);
+        console.log(num);
+
+        seeMore = document.querySelectorAll(".button-1")[num];
+        article = document.querySelectorAll(".see-moree")[num];
+
+        article.classList.toggle("expanded");
+        
+        const expanded = article.classList.contains("expanded");
+        if (expanded) {
+                seeMore.innerHTML = "See Less";
+            } else {
+                seeMore.innerHTML = "See More";
+            }
+        }
+};
