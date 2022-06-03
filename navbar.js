3e963998dbd8399e56897331302f164c4b2ec1ff
Gtadia <m235humit9@gmail.com>
2022-06-03 00:30:53 -0400

    FINALLY FINISHED THE NAVIGATION BAR

--- navbar.js
+++ navbar.js
@@ -1,6 +1,23 @@
-window.addEventListener('scroll', function () {
-    let header = document.querySelector('.stuff');
-    let windowPosition = window.scrollY > 100;
-    header.classList.toggle('scrolling-active', windowPosition);
-})
+const menus = document.querySelectorAll('.menu')
+const dropdown = document.querySelector('.dropdown')
+let activeMenu = null
 
+menus.forEach(menu => {
+  menu.addEventListener('click', e => {
+    // Removing previous active menu that is not itself
+    if (activeMenu && activeMenu !== menu) {
+      activeMenu.classList.remove('clicked')
+      activeMenu = menu
+    }
+    else if (activeMenu && activeMenu === menu) {
+      activeMenu = null
+    } else {
+      activeMenu = menu
+    }
+    
+    menu.classList.toggle('clicked')
+    // If there is an active menu, show
+    if (activeMenu) dropdown.classList.add('show')
+    else dropdown.classList.remove('show')
+  })
+})
